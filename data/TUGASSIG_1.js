6567c4848b15d7e55a01060616e5f9d99a7f1e6e
AURBCA <naufamotovlog03@gmail.com>
2024-12-02 21:27:26 +0700

    Add files via upload

---
+++ data/TUGASSIG_1.js
@@ -0,0 +1,19 @@
+var json_TUGASSIG_1 = {
+"type": "FeatureCollection",
+"name": "TUGASSIG_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "LOKASI": "Mas Octo ", "x": -7.7662, "y": 110.40443 }, "geometry": { "type": "Point", "coordinates": [ 110.4044271, -7.76620012 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Aufa", "x": -7.73382, "y": 110.45626 }, "geometry": { "type": "Point", "coordinates": [ 110.4562587, -7.733815798 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Aldric ", "x": -7.79632, "y": 110.33357 }, "geometry": { "type": "Point", "coordinates": [ 110.3335687, -7.7963217 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "⁠Dinda ", "x": -7.76418, "y": 110.41505 }, "geometry": { "type": "Point", "coordinates": [ 110.4150522, -7.764175519 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Zahra ", "x": -7.80512, "y": 110.45394 }, "geometry": { "type": "Point", "coordinates": [ 110.4539442, -7.8051156 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Nanta ", "x": -7.78932, "y": 110.32933 }, "geometry": { "type": "Point", "coordinates": [ 110.3293332, -7.7893206 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Akhsan", "x": -7.82809, "y": 110.41632 }, "geometry": { "type": "Point", "coordinates": [ 110.416321, -7.82809 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Mba Adel", "x": -7.77753, "y": 110.41723 }, "geometry": { "type": "Point", "coordinates": [ 110.417229, -7.777531 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Grace", "x": -7.76918, "y": 110.40795 }, "geometry": { "type": "Point", "coordinates": [ 110.4079455, -7.769175539 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Fero", "x": -7.7687, "y": 110.40931 }, "geometry": { "type": "Point", "coordinates": [ 110.4093059, -7.7687035 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Nabeel", "x": -7.76548, "y": 110.40018 }, "geometry": { "type": "Point", "coordinates": [ 110.40018, -7.7654752 ] } },
+{ "type": "Feature", "properties": { "LOKASI": "Raihan", "x": -7.76491, "y": 110.41705 }, "geometry": { "type": "Point", "coordinates": [ 110.4170504, -7.764913766 ] } }
+]
+}
